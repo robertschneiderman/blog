e096e2ee0bb9d26b245a6199b65354931afecee0
Robert Schneiderman <Rob@Roberts-MacBook-Pro-2.local>
2016-10-29 23:15:45 -0700

    laid groundwork for regular theme

--- wp-content/themes/schneiderman_theme/header.php
+++ wp-content/themes/schneiderman_theme/header.php
@@ -10,14 +10,19 @@
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
 <section id="branding">
-<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
+<!-- <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div> -->
+<!-- <div id="site-description"><?php bloginfo( 'description' ); ?></div> -->
 </section>
 <nav id="menu" role="navigation">
 <div id="search">
-<?php get_search_form(); ?>
+<?php
+  // get_search_form();
+?>
 </div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+<?php
+  // wp_nav_menu( array( 'theme_location' => 'main-menu' ) );
+?>
 </nav>
 </header>
 <div id="container">
