2b2da519ee3503337087074e3b7569434c8f46bd
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-13 23:02:33 -0800

    adding final styling to theme

--- wp-content/themes/schneiderman/entry-meta.php
+++ wp-content/themes/schneiderman/entry-meta.php
@@ -1,13 +1,10 @@
 <section class="entry-meta">
-<div class="author">
-  <img class="author-avatar" src="http://d4fd8edb5357e3663250-e9db5fe345a903020dd4e30b554d328e.r74.cf5.rackcdn.com/headshot_square.jpg" alt="">
-  <p class="author-name">
-  <?php echo get_the_author_meta('first_name', $post->post_author) . ' ' . get_the_author_meta('last_name', $post->post_author); ?>
-  </p>
+<div class="ibm">
+  <span class="comments fs-two-half">
+    <?php comments_number(0) ?>
+  </span>
+  <img class="two-half ibm" src="http://d4fd8edb5357e3663250-e9db5fe345a903020dd4e30b554d328e.r74.cf5.rackcdn.com/chat.svg" alt="">
 </div>
-<span class="comments">
-  <?php comments_number() ?>
-</span>
 <span class="meta-sep"> | </span>
 <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 </section>
